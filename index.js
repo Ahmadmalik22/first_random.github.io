9d1b016bdcdad29f1b1948610152b9ad4cd22b6f
Ahmadmalik22 <135091373+Ahmadmalik22@users.noreply.github.com>
2023-12-29 12:44:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const ul=document.querySelector("ul");
+const hamburger=document.querySelector(".hamburger")
+const top_in=document.querySelector(".top_in")
+const body=document.querySelector("main")
+ham_icon_control=true;
+hamburger.addEventListener("click",()=>{
+if(ham_icon_control){
+    // top_in.style.top="72px"
+    ul.classList.add("top_in")
+body.style.backgroundImage="linear-gradient(to right,rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url(./bg.webp)"
+hamburger.innerHTML='<i class="fa-solid fa-xmark"></i>'
+ham_icon_control=false;}
+else {
+    ham_icon_control=true;
+    hamburger.innerHTML='<i class="fa-solid fa-bars"></i>'
+    ul.classList.remove("top_in")
+body.style.backgroundImage="url(./bg.webp)"
+}
+})
